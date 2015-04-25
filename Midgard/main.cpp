043697b0401fa12de6c2fb6d5608aeb9d3560b3f
luisrbrenes <luisrbrenes@luisrbrenes>
2015-04-24 19:57:34 -0600

    Commit inicial

---
+++ Midgard/main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   main.cpp
+ * Author: luisrbrenes
+ *
+ * Created on April 24, 2015, 7:57 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    
+    //prueba
+    return 0;
+}
+
